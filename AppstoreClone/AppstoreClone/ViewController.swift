81ba3bde150408df6c3bb9c104e97d296724518e
JongPyoAhn <whdvy95@gmail.com>
2023-04-12 18:57:54 +0900

    프로젝트 시작 no storyBoard

---
+++ AppstoreClone/AppstoreClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppstoreClone
+//
+//  Created by 안종표 on 2023/04/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
