1efa3e39ca2d7155645699ad346e5a9f7a79c8f8
JongPyoAhn <whdvy95@gmail.com>
2023-04-12 19:56:10 +0900

    TabbarItem 설정

--- AppstoreClone/AppstoreClone/TabBarController.swift
+++ AppstoreClone/AppstoreClone/TabBarController.swift
@@ -11,11 +11,15 @@ class TabBarController: UITabBarController {
     
     private lazy var todayViewController: UIViewController = {
         let viewController = UIViewController()
+        let tabBarItem = UITabBarItem(title: "투데이", image: UIImage(systemName: "mail"), tag: 0)
+        viewController.tabBarItem = tabBarItem
         return viewController
     }()
     
     private lazy var appViewController: UIViewController = {
         let viewController = UIViewController()
+        let tabBarItem = UITabBarItem(title: "앱", image: UIImage(systemName: "square.stack.3d.up"), tag: 1)
+        viewController.tabBarItem = tabBarItem
         return viewController
     }()
 
