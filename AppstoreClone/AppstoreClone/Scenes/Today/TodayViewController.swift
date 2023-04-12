46dfa3962c2b8fffc6c3c8f8d654df3f1485270f
JongPyoAhn <whdvy95@gmail.com>
2023-04-12 20:11:28 +0900

    CollectionView Delegate, Datasource

--- AppstoreClone/AppstoreClone/Scenes/Today/TodayViewController.swift
+++ AppstoreClone/AppstoreClone/Scenes/Today/TodayViewController.swift
@@ -4,12 +4,52 @@
 //
 //  Created by 안종표 on 2023/04/12.
 //
-
+import SnapKit
 import UIKit
 final class TodayViewController: UIViewController{
-    
+    private lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        collectionView.backgroundColor = .systemBackground
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "todayCell")
+        
+        return collectionView
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureUI()
+        
+    }
+    func configureUI(){
+        self.view.addSubview(self.collectionView)
+        self.collectionView.snp.makeConstraints{
+            $0.edges.equalToSuperview()
+        }
+    }
+}
+
+
+
+extension TodayViewController: UICollectionViewDataSource{
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "todayCell", for: indexPath)
+        cell.backgroundColor = .black
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        5
+    }
+}
+
+extension TodayViewController: UICollectionViewDelegateFlowLayout{
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = self.collectionView.frame.width - 32.0
+        return CGSize(width: width, height: width)
     }
     
 }
