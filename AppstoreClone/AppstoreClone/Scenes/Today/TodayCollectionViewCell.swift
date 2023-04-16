517e9e1514f52a55840001875de51e69f5f0215f
JongPyoAhn <whdvy95@gmail.com>
2023-04-16 12:58:17 +0900

    이미지 다운샘플링 적용

--- AppstoreClone/AppstoreClone/Scenes/Today/TodayCollectionViewCell.swift
+++ AppstoreClone/AppstoreClone/Scenes/Today/TodayCollectionViewCell.swift
@@ -71,7 +71,7 @@ final class TodayCollectionViewCell: UICollectionViewCell{
             if let error = error{
                 fatalError(error.localizedDescription)
             }
-            if let data = data, let image = UIImage(data: data) {
+            if let data = data, let image = self.downsample(imageAt: data, to: CGSize(width: 300, height: 300)) {
                 self.imageCache.setObject(image, forKey: "\(url)" as NSString)
                 DispatchQueue.main.async {
                     self.imageView.image = image
@@ -79,6 +79,30 @@ final class TodayCollectionViewCell: UICollectionViewCell{
             }
         }
         self.task.resume()
+    }
+}
+extension TodayCollectionViewCell {
+    func downsample(imageAt imageData: Data, to pointSize: CGSize) -> UIImage? {
+        // 이미지 속성을 가져옵니다.
+        let options = [kCGImageSourceShouldCache: false] as CFDictionary
+        guard let source = CGImageSourceCreateWithData(imageData as CFData, options) else {
+            return nil
+        }
+        // 다운샘플링할 이미지의 최대 크기를 픽셀로 계산합니다.
+        let maxDimensionInPixels = max(pointSize.width, pointSize.height) * UIScreen.main.scale
+        // 다운샘플링 옵션을 설정합니다.
+        let downsampleOptions = [
+            kCGImageSourceCreateThumbnailFromImageAlways: true,
+            kCGImageSourceShouldCacheImmediately: true,
+            kCGImageSourceCreateThumbnailWithTransform: true,
+            kCGImageSourceThumbnailMaxPixelSize: maxDimensionInPixels
+        ] as CFDictionary
+        // 이미지를 다운샘플링합니다.
+        guard let downsampledImage = CGImageSourceCreateThumbnailAtIndex(source, 0, downsampleOptions) else {
+            return nil
+        }
+        // 다운샘플링된 이미지를 UIImage로 변환합니다.
+        return UIImage(cgImage: downsampledImage)
     }
 }
 private extension TodayCollectionViewCell{
